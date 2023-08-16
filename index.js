2097317bd3364f4fd5f9141f4c16bfb81052f634
gmarvis <sgmarvis@gmail.com>
2023-08-16 16:24:02 +0100

    js linta resolved

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-let wordOne = document.getElementById("word-one");
-let wordTwo = document.getElementById("word-two");
-const btn = document.getElementById("btn");
+const wordOne = document.getElementById('word-one')
+const wordTwo = document.getElementById('word-two')
+const btn = document.getElementById('btn')
 
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-  isIsomophic();
-});
+btn.addEventListener('click', (event) => {
+  event.preventDefault()
+  isIsomophic()
+})
 
 const isIsomophic = (s, t) => {
-  s = wordOne.value;
-  t = wordTwo.value;
+  s = wordOne.value
+  t = wordTwo.value
 
   if (!s || !t) {
-    document.getElementById("status").innerText = "please fill all inputs";
-    document.getElementById("status").style.color = "red";
-    return;
+    document.getElementById('status').innerText = 'please fill all inputs'
+    document.getElementById('status').style.color = 'red'
+    return
   }
 
   if (s.length !== t.length) {
-    document.getElementById("status").innerText =
-      "please enter words of the same length";
-    document.getElementById("status").style.color = "red";
-    console.log("please enter words of the same length", s, t);
-    return;
+    document.getElementById('status').innerText =
+      'please enter words of the same length'
+    document.getElementById('status').style.color = 'red'
+    console.log('please enter words of the same length', s, t)
+    return
   }
-  const map = {};
+  const map = {}
   for (let i = 0; i < s.length; i++) {
-    const firstLatterOfS = s[i];
-    const firstLatterOft = t[i];
+    const firstLatterOfS = s[i]
+    const firstLatterOft = t[i]
     if (map[firstLatterOfS] === undefined) {
-      map[firstLatterOfS] = firstLatterOft;
+      map[firstLatterOfS] = firstLatterOft
     } else if (map[firstLatterOfS] !== firstLatterOft) {
-      document.getElementById("status").innerText = "false";
-      document.getElementById("status").style.color = "red";
-      return;
+      document.getElementById('status').innerText = 'false'
+      document.getElementById('status').style.color = 'red'
+      return
     }
   }
-  document.getElementById("status").innerText = "true";
-  document.getElementById("status").style.color = "green";
-  return;
-};
+  document.getElementById('status').innerText = 'true'
+  document.getElementById('status').style.color = 'green'
+}
